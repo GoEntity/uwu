4186ecc4bfee2709f5782a63b886d573fe2bb1ac
GoEntity <javaLearner13@gmail.com>
2023-06-21 21:27:26 +0900

    .

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const blogPosts = document.getElementById('posts');
 
     let posts = [
-        { title: 'rando post', content: 'GoEntity' },
-        { title: 'rando 2', content: 'hi' },
+        { title: 'Posts', content: 'hi.. whats up.........' },
+        // { title: 'rando 2', content: 'hi' },
     ];
 
     posts.forEach(function(post) {
         let postElement = document.createElement('article');
-        let titleElement = document.createElement('h2');
-        let contentElement = document.createElement('p');
+        
+        let titleElement = document.createElement('h3');
+        let contentElement = document.createElement('h5');
 
         titleElement.textContent = post.title;
         contentElement.textContent = post.content;
