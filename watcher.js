961267cbf0b7242a722018ca77584116046bb1cf
GoEntity <javaLearner13@gmail.com>
2023-06-21 21:58:25 +0900

    why

--- watcher.js
+++ watcher.js
@@ -1,5 +1,7 @@
 const chokidar = require('chokidar');
-const git = require('simple-git');
+const git = require('simple-git')({
+    baseDir: 'C:\\GoEntity',
+});
 
 const watcher = chokidar.watch('C:\\GoEntity', {
   ignored: /(^|[\/\\])\../, // ignore dotfiles
@@ -12,12 +14,14 @@ watcher
     git()
       .add('./*')
       .commit("auto commit (::file changed)")
-      .push('origin', 'main', () => console.log('push successful'));
+      .push('origin', 'main', () => console.log('push successful'))
+      .catch((err) => console.error('error: ', err));
   })
   .on('add', (path) => {
     console.log('File', path, 'has been added');
     git()
       .add('./*')
       .commit("auto commit (::file added)")
-      .push('origin', 'main', () => console.log('push successful'));
+      .push('origin', 'main', () => console.log('push successful'))
+      .catch((err) => console.error('error: ', err));
   });
