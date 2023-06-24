87796851211ccb24ed7605b1116ffddafdc19cc9
GoEntity <javaLearner13@gmail.com>
2023-06-25 07:01:38 +0900

    _________________________________________GoEntity is currently OFFLINE

--- online.bat
+++ online.bat
@@ -1,6 +1,7 @@
 @echo off
 cd E:\Git\GoEntity
 git pull origin main
+git push --set-upstream origin main
 echo > 000_STATUS_000
 git add 000_STATUS_000
 git commit -m "_________________________________________GoEntity is currently ONLINE"
