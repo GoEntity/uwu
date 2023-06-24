b8ff534521622f66ce6314d364e8a61f69834f99
GoEntity <javaLearner13@gmail.com>
2023-06-25 07:37:20 +0900

    auto commit (::file change)

--- watcher.js
+++ watcher.js
@@ -6,7 +6,7 @@ const git = simpleGit();
 git.cwd('E:\\Git\\GoEntity');
 
 const watcher = chokidar.watch('E:\\Git\\GoEntity', {
-  ignored: [/(^|[\/\\])\..|node_modules/, /offline\.bat/, /online\.bat$/],
+  ignored: [/(^|[\/\\])\..|node_modules/, /offline\.bat/, /online\.bat/],
   persistent: true
 });
 
