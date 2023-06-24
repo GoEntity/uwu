5dbd9aaf1bb975d0df18f871e1f287b37cac18ad
GoEntity <javaLearner13@gmail.com>
2023-06-25 06:19:28 +0900

    HOW ARE YOU TODAY?

--- autoCommit.bat
+++ autoCommit.bat
@@ -1,12 +1,12 @@
 @echo off
 cd E:\Git\GoEntity
 git push --set-upstream origin main
-echo > file.txt
+echo > HI_I_WILL_BE_GONE_IN_5_MINS
 git add .
-git commit -m "Add file.txt"
+git commit -m "HOW ARE YOU TODAY?"
 git push
 timeout /t 300
 del file.txt
 git add .
-git commit -m "Delete file.txt"
+git commit -m "SEE YOU SOON!"
 git push
