b1c8ca72e388ea49b936fdd9a82320a761563fba
GoEntity <javaLearner13@gmail.com>
2023-06-25 06:26:37 +0900

    HOW ARE YOU TODAY?

--- autoCommit.bat
+++ autoCommit.bat
@@ -5,8 +5,8 @@ echo > HI_I_WILL_BE_GONE_IN_5_MINS
 git add .
 git commit -m "HOW ARE YOU TODAY?"
 git push
-timeout /t 300
-del file.txt
+timeout /t 
+del HI_I_WILL_BE_GONE_IN_5_MINS
 git add .
 git commit -m "SEE YOU SOON!"
 git push
