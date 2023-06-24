790783d59ce39aae3a13465f3ba0ca7d9eca1f9c
GoEntity <javaLearner13@gmail.com>
2023-06-25 06:14:14 +0900

    Add file.txt

--- autoCommit.bat
+++ autoCommit.bat
@@ -1,11 +1,11 @@
 @echo off
 cd E:\Git\GoEntity
-echo > hi i will be gone in 5 mins.txt
+echo > file.txt
 git add .
-git commit -m "okay see you soon later"
+git commit -m "Add file.txt"
 git push
 timeout /t 300
 del file.txt
 git add .
-git commit -m "poof!"
+git commit -m "Delete file.txt"
 git push
