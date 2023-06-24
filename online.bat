b99302f3af346b2959e1eeff2da49a92728fa9f4
GoEntity <javaLearner13@gmail.com>
2023-06-25 07:38:59 +0900

    batch to show online/offline :)

---
+++ online.bat
@@ -0,0 +1,9 @@
+@echo off
+cd E:\Git\GoEntity
+git pull origin main
+
+echo HI IM ONLINE > lllllllllllllllllllllllllllllllll
+git add lllllllllllllllllllllllllllllllll
+git reset -- online.bat
+git commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
+git push
