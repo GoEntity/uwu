bc2314aa31f61694dfde5ea0d0f507d13b9e10c5
GoEntity <javaLearner13@gmail.com>
2023-06-25 07:53:42 +0900

    fix

--- online.bat
+++ online.bat
@@ -2,8 +2,20 @@
 cd E:\Git\GoEntity
 git pull origin main
 
-echo HI IM ONLINE > lllllllllllllllllllllllllllllllll
-git add lllllllllllllllllllllllllllllllll
+echo HI IM ONLINE > __________llllll__________
+echo HI IM ONLINE > _________lll__lll_________
+echo HI IM ONLINE > ________lll____lll________
+echo HI IM ONLINE > ________lll____lll________
+echo HI IM ONLINE > _________lll__lll_________
+echo HI IM ONLINE > __________llllll__________
+
+git add __________llllll__________
+git add _________lll__lll_________
+git add ________lll____lll________
+git add ________lll____lll________
+git add _________lll__lll_________
+git add __________llllll__________
+
 git reset -- online.bat
 git commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
 git push
