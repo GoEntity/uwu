8f93a7bca759e272f5070fa47e13a3d7bdad4a66
GoEntity <javaLearner13@gmail.com>
2023-06-25 07:36:00 +0900

    auto commit (::file unlink)

--- offline.bat
+++ offline.bat
@@ -2,8 +2,8 @@
 cd E:\Git\GoEntity
 git pull origin main
 
-echo HI IM OFFLINE > llllllllllllllllll
-git add llllllllllllllllll
+echo HI IM OFFLINE > lllllllllllllllllllllllllllllllll
+git add lllllllllllllllllllllllllllllllll
 git reset -- offline.bat
 git commit -m "<< ONLINE STATUS >>_______________GoEntity is currently OFFLINE :("
 git push
--- online.bat
+++ online.bat
@@ -2,8 +2,8 @@
 cd E:\Git\GoEntity
 git pull origin main
 
-echo HI IM ONLINE > llllllllllllllllll
-git add llllllllllllllllll
+echo HI IM ONLINE > lllllllllllllllllllllllllllllllll
+git add lllllllllllllllllllllllllllllllll
 git reset -- online.bat
 git commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
 git push
