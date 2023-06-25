4529bcce7b43257aeff7067638109e33a2cc8352
GoEntity <javaLearner13@gmail.com>
2023-06-25 11:18:03 +0900

    auto commit (::file add)

--- offline.bat
+++ offline.bat
@@ -25,7 +25,7 @@ echo OFFLINE > E:\Git\GoEntity\000000
 echo OFFLINE > E:\Git\GoEntity\00000
 echo OFFLINE > E:\Git\GoEntity\0000
 echo OFFLINE > E:\Git\GoEntity\000
-echo HI IM OFFLINE > E:\Git\GoEntity\00
+echo OFFLINE > E:\Git\GoEntity\00
 echo HI IM OFFLINE > E:\Git\GoEntity\0
 
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
