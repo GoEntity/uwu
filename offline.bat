0a46b2c1d686440b271f510747b3db91237a6505
GoEntity <javaLearner13@gmail.com>
2023-06-25 12:20:13 +0900

    auto commit (::file addDir)

--- offline.bat
+++ offline.bat
@@ -30,6 +30,7 @@ echo OFFLINE > E:\Git\GoEntity\0
 
 echo %date% %time% > E:\Git\GoEntity\timePassed
 
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
--- online.bat
+++ online.bat
@@ -30,6 +30,7 @@ echo ONLINE > E:\Git\GoEntity\0
 
 echo %date% %time% > E:\Git\GoEntity\timePassed
 
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
