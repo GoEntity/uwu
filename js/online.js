df715e7bfd6d294182365af9a1e502a503ab1993
GoEntity <javaLearner13@gmail.com>
2023-06-25 11:39:45 +0900

    auto commit (::file change)

--- js/online.js
+++ js/online.js
@@ -15,7 +15,7 @@ fetch('0')
         console.error('Error:', error);
     });
 
-if fetch('onlineTime')
+fetch('onlineTime')
     .then(response => response.text())
     .then(data => {
         let onlineTime = new Date(data);
