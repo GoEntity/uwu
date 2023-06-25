1a308f1e50f1a241a3e7cdc27f74208e7f3b1062
GoEntity <goentity13@gmail.com>
2023-06-25 15:15:02 +0900

    wow what happened to my pc

--- online.bat
+++ online.bat
@@ -28,15 +28,35 @@ echo ONLINE > E:\Git\GoEntity\000
 echo ONLINE > E:\Git\GoEntity\00
 echo ONLINE > E:\Git\GoEntity\0
 
-for %%i in (0 00 000 0000 00000 000000 0000000 00000000 000000000 0000000000 00000000000 000000000000 0000000000000 00000000000000 000000000000000 0000000000000000 00000000000000000 000000000000000000 0000000000000000000 00000000000000000000 000000000000000000000 0000000000000000000000 00000000000000000000000 000000000000000000000000) do "C:\Git\bin\git.exe" add E:\Git\GoEntity\%%i
-
-"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>___________GoEntity is currently ONLINE :)"
-"C:\Git\bin\git.exe" push
-
 echo %date% %time% > E:\Git\GoEntity\timePassed
 
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
-"C:\Git\bin\git.exe" commit -m "spying on GoEntity..."
-"C:\Git\bin\git.exe" push
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0
 
-shutdown /s /t 0
+"C:\Git\bin\git.exe" reset -- online.bat
+"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
+"C:\Git\bin\git.exe" push
