cdb9544fee65af6e637b39c6300ab4d769ea41a7
GoEntity <goentity13@gmail.com>
2023-06-25 16:00:58 +0900

    auto commit (::file addDir)

--- offline.bat
+++ offline.bat
@@ -28,8 +28,6 @@ echo OFFLINE > E:\Git\GoEntity\000
 echo OFFLINE > E:\Git\GoEntity\00
 echo OFFLINE > E:\Git\GoEntity\0
 
-echo %date% %time% > E:\Git\GoEntity\timePassed
-
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
@@ -59,8 +57,10 @@ echo %date% %time% > E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" reset -- offline.bat
 "C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently OFFLINE :("
 
+echo %date% %time% > E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" commit -m "spying on GoEntity..."
+
 "C:\Git\bin\git.exe" push origin main
 
 shutdown /s /t 0
--- online.bat
+++ online.bat
@@ -28,8 +28,6 @@ echo ONLINE > E:\Git\GoEntity\000
 echo ONLINE > E:\Git\GoEntity\00
 echo ONLINE > E:\Git\GoEntity\0
 
-echo %date% %time% > E:\Git\GoEntity\timePassed
-
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
@@ -60,6 +58,8 @@ echo %date% %time% > E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" reset -- online.bat
 "C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
 
+echo %date% %time% > E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
 "C:\Git\bin\git.exe" commit -m "spying on GoEntity..."
+
 "C:\Git\bin\git.exe" push origin main
