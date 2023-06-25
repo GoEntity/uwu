54a50418b82e3fe929a8614c12f0f9be1e79ea06
GoEntity <javaLearner13@gmail.com>
2023-06-25 12:30:14 +0900

    .

--- js/online.js
+++ js/online.js
@@ -1,39 +1,39 @@
-fetch('0')
-    .then(response => response.text())
-    .then(data => {
-        let statusElement = document.getElementById('status');
-        statusElement.textContent = data;
+function updateStatus() {
+    fetch('0?' + new Date().getTime())
+        .then(response => response.text())
+        .then(data => {
+            let statusElement = document.getElementById('status');
+            statusElement.textContent = data;
 
-        if (data.includes('ONLINE')) {
-            statusElement.style.color = 'rgb(6, 190, 0)';
-        } else if (data.includes('OFFLINE')) {
-            statusElement.style.color = 'rgb(158, 0, 0)';
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+            if (data.includes('ONLINE')) {
+                statusElement.style.color = 'rgb(6, 190, 0)';
+            } else if (data.includes('OFFLINE')) {
+                statusElement.style.color = 'rgb(158, 0, 0)';
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            document.getElementById('status').textContent = `Fetch error: ${error}`;
+        });
+}
 
-fetch('timePassed')
-    .then(response => response.text())
-    .then(data => {
-        let onlineTime = new Date(data);
-        let currentTime = new Date();
-        let minutesPassed = Math.round((currentTime - onlineTime) / 60000);
-        document.getElementById('timePassed').textContent = `since ${minutesPassed} minutes ago`;
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+function updateTimePassed() {
+    fetch('timePassed?' + new Date().getTime())
+        .then(response => response.text())
+        .then(data => {
+            let onlineTime = new Date(data);
+            let currentTime = new Date();
+            let minutesPassed = Math.round((currentTime - onlineTime) / 60000);
+            document.getElementById('timePassed').textContent = `since ${minutesPassed} minutes ago`;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            document.getElementById('timePassed').textContent = `Fetch error: ${error}`;
+        });
+}
 
-// fetch('offlineTime')
-//     .then(response => response.text())
-//     .then(data => {
-//         let offlineTime = new Date(data);
-//         let currentTime = new Date();
-//         let minutesPassed = Math.round((currentTime - offlineTime) / 60000);
-//         document.getElementById('offlineTime').textContent = `since ${minutesPassed} minutes ago`;
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
+updateStatus();
+updateTimePassed();
+
+setInterval(updateStatus, 60000);
+setInterval(updateTimePassed, 60000);
