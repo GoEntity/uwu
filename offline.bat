b5f2d8d2bf5d6c30a05ab040ed5925bd0ff0557c
GoEntity <javaLearner13@gmail.com>
2023-06-25 12:52:39 +0900

    batch status/time

--- offline.bat
+++ offline.bat
@@ -28,37 +28,15 @@ echo OFFLINE > E:\Git\GoEntity\000
 echo OFFLINE > E:\Git\GoEntity\00
 echo OFFLINE > E:\Git\GoEntity\0
 
+for %%i in (0 00 000 0000 00000 000000 0000000 00000000 000000000 0000000000 00000000000 000000000000 0000000000000 00000000000000 000000000000000 0000000000000000 00000000000000000 000000000000000000 0000000000000000000 00000000000000000000 000000000000000000000 0000000000000000000000 00000000000000000000000 000000000000000000000000) do "C:\Git\bin\git.exe" add E:\Git\GoEntity\%%i
+
+"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>___________GoEntity is currently OFFLINE :("
+"C:\Git\bin\git.exe" push
+
 echo %date% %time% > E:\Git\GoEntity\timePassed
 
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\timePassed
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\000
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\00
-"C:\Git\bin\git.exe" add E:\Git\GoEntity\0
-
-"C:\Git\bin\git.exe" reset -- offline.bat
-"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently OFFLINE :("
+"C:\Git\bin\git.exe" commit -m "spying on GoEntity..."
 "C:\Git\bin\git.exe" push
 
 shutdown /s /t 0
