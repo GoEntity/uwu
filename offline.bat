b86f7fdb26d34b87502e3d10c267264de4d8ec2c
GoEntity <goentity13@gmail.com>
2023-06-27 03:54:57 +0900

    argh!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- offline.bat
+++ offline.bat
@@ -3,8 +3,18 @@ cd E:\Git\GoEntity
 "C:\Git\bin\git.exe" pull origin main
 
 echo OFFLINE > E:\Git\GoEntity\0000000000000000000000000
+echo OFFLINE > E:\Git\GoEntity\000000000000000000000000
+echo OFFLINE > E:\Git\GoEntity\00000000000000000000000
+echo OFFLINE > E:\Git\GoEntity\0000000000000000000000
+echo OFFLINE > E:\Git\GoEntity\000000000000000000000
+echo OFFLINE > E:\Git\GoEntity\00000000000000000000
 
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000
 
 "C:\Git\bin\git.exe" reset -- offline.bat
 "C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently OFFLINE :("
--- online.bat
+++ online.bat
@@ -3,8 +3,18 @@ cd E:\Git\GoEntity
 "C:\Git\bin\git.exe" pull origin main
 
 echo ONLINE > E:\Git\GoEntity\0000000000000000000000000
+echo ONLINE > E:\Git\GoEntity\000000000000000000000000
+echo ONLINE > E:\Git\GoEntity\00000000000000000000000
+echo ONLINE > E:\Git\GoEntity\0000000000000000000000
+echo ONLINE > E:\Git\GoEntity\000000000000000000000
+echo ONLINE > E:\Git\GoEntity\00000000000000000000
 
 "C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\0000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\GoEntity\00000000000000000000
 
 "C:\Git\bin\git.exe" reset -- online.bat
 "C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently ONLINE :)"
