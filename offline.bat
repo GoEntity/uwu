579e27051a001aacac39defd8dfdeb6e9b39e2a8
GoEntity <goentity13@gmail.com>
2023-07-01 06:32:35 +0900

    uwu

--- offline.bat
+++ offline.bat
@@ -1,26 +1,26 @@
 @echo off
-cd E:\Git\auto_git_push_for_my_blog
+cd E:\Git\uwu
 "C:\Git\bin\git.exe" pull origin main
 
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\0000000000000000000000000
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\000000000000000000000000
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\00000000000000000000000
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\0000000000000000000000
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\000000000000000000000
-echo OFFLINE > E:\Git\auto_git_push_for_my_blog\00000000000000000000
+echo OFFLINE > E:\Git\uwu\0000000000000000000000000
+echo OFFLINE > E:\Git\uwu\000000000000000000000000
+echo OFFLINE > E:\Git\uwu\00000000000000000000000
+echo OFFLINE > E:\Git\uwu\0000000000000000000000
+echo OFFLINE > E:\Git\uwu\000000000000000000000
+echo OFFLINE > E:\Git\uwu\00000000000000000000
 
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\0000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\00000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\0000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\00000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\0000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\00000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\0000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\00000000000000000000
 
 "C:\Git\bin\git.exe" reset -- offline.bat
 "C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________GoEntity is currently OFFLINE :("
 
-echo %date% %time% > E:\Git\auto_git_push_for_my_blog\timePassed
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\timePassed
+echo %date% %time% > E:\Git\uwu\timePassed
+"C:\Git\bin\git.exe" add E:\Git\uwu\timePassed
 "C:\Git\bin\git.exe" commit -m "spying on GoEntity..."
 
 "C:\Git\bin\git.exe" push origin main
--- online.bat
+++ online.bat
@@ -1,26 +1,26 @@
 @echo off
-cd E:\Git\auto_git_push_for_my_blog
+cd E:\Git\uwu
 "C:\Git\bin\git.exe" pull origin main
 
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\0000000000000000000000000
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\000000000000000000000000
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\00000000000000000000000
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\0000000000000000000000
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\000000000000000000000
-echo ONLINE > E:\Git\auto_git_push_for_my_blog\00000000000000000000
+echo ONLINE > E:\Git\uwu\0000000000000000000000000
+echo ONLINE > E:\Git\uwu\000000000000000000000000
+echo ONLINE > E:\Git\uwu\00000000000000000000000
+echo ONLINE > E:\Git\uwu\0000000000000000000000
+echo ONLINE > E:\Git\uwu\000000000000000000000
+echo ONLINE > E:\Git\uwu\00000000000000000000
 
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\0000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\000000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\00000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\0000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\000000000000000000000
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\00000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\0000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\000000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\00000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\0000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\000000000000000000000
+"C:\Git\bin\git.exe" add E:\Git\uwu\00000000000000000000
 
 "C:\Git\bin\git.exe" reset -- online.bat
-"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________auto_git_push_for_my_blog is currently ONLINE :)"
+"C:\Git\bin\git.exe" commit -m "<< ONLINE STATUS >>_______________uwu is currently ONLINE :)"
 
-echo %date% %time% > E:\Git\auto_git_push_for_my_blog\timePassed
-"C:\Git\bin\git.exe" add E:\Git\auto_git_push_for_my_blog\timePassed
+echo %date% %time% > E:\Git\uwu\timePassed
+"C:\Git\bin\git.exe" add E:\Git\uwu\timePassed
 "C:\Git\bin\git.exe" commit -m "spying on GoEntity.............."
 
 "C:\Git\bin\git.exe" push origin main
